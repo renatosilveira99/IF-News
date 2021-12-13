1d412d2278c2d14be2ba487341f1b2d158bc5514
renatosilveira-codeshop <79472393+renatosilveira-codeshop@users.noreply.github.com>
2021-12-13 19:42:10 -0300

    feat: migrations, docker and database config

---
+++ src/database/migrations/1639432538514-CreateRegistrationsRequests.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateRegistrationsRequests1639432538514 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'registrations_requests',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'approverId',
+                        type: 'uuid',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'userId',
+                        type: 'uuid',
+                    },
+                    {
+                        name: 'status',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: 'fk_registration_request_userId_users',
+                        referencedTableName: 'users',
+                        referencedColumnNames: ['id'],
+                        columnNames: ['userId'],
+                    },
+                    {
+                        name: 'fk_registration_request_approverId_users',
+                        referencedTableName: 'users',
+                        referencedColumnNames: ['id'],
+                        columnNames: ['approverId'],
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('registrations_requests');
+    }
+}
