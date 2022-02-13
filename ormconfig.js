5ed5a65153a30128bf16036112f0dbd5b58c5e3f
renatosilveira-codeshop <43325147+renatosilveira99@users.noreply.github.com>
2022-02-12 22:05:41 -0300

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,22 @@
 module.exports = {
-  "type": "postgres",
-  "url": process.env.DATABASE_URL,
-  "entities": ["dist/src/modules/**/entities/*.js"],
-  "migrations": ["dist/src/database/migrations/**/*.js"],
-  "cli": {
-    "entitiesDir": "dist/src/modules/**/entities",
-    "migrationsDir": "dist/src/database/migrations",
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  dropSchema: false,
+  logging: true,
+  ssl: {
+    ca: process.env.SSL_CERT,
+  },
+  entities: ["dist/src/modules/**/entities/*.js"],
+  migrations: ["dist/src/database/migrations/**/*.js"],
+  cli: {
+    entitiesDir: "dist/src/modules/**/entities",
+    migrationsDir: "dist/src/database/migrations",
+  },
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 };
