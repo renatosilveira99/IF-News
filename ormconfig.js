b560a293d6b59f64df4ec5a49cae429b33a9f4bf
renatosilveira-codeshop <43325147+renatosilveira99@users.noreply.github.com>
2022-02-12 21:57:31 -0300

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "entities": ["dist/src/modules/**/entities/*.js"],
-  "migrations": ["dist/src/migrations/**/*.js"],
+  "migrations": ["dist/src/database/migrations/**/*.js"],
   "cli": {
     "entitiesDir": "dist/src/modules/**/entities",
     "migrationsDir": "dist/src/database/migrations",
