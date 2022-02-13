0c96dac4df14f96dcc2023acec22faa62ce8c835
renatosilveira-codeshop <43325147+renatosilveira99@users.noreply.github.com>
2022-02-12 21:55:46 -0300

    Update database connection

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,6 @@
 module.exports = {
-  "type": process.env.TYPEORM_CONNECTION,
-  "host": process.env.TYPEORM_HOST,
-  "port": process.env.TYPEORM_PORT,
-  "username": process.env.TYPEORM_USERNAME,
-  "password": process.env.TYPEORM_PASSWORD,
-  "database": process.env.TYPEORM_DATABASE,
-  "synchronize": true,
-  "logging": false,
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
   "entities": ["dist/src/modules/**/entities/*.js"],
   "migrations": ["dist/src/migrations/**/*.js"],
   "cli": {
