dac52a972a03de6c4ff9ec8305295bfb952960a3
renatosilveira-codeshop <43325147+renatosilveira99@users.noreply.github.com>
2022-03-06 21:15:15 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -36,8 +36,7 @@ app.use(
 
     return response.status(500).json({
       message:
-        `Internal server error - ${error.message}`,
-      requestBody: request.file,
+        `Internal server error - ${error.message} - ${JSON.stringify(request.file)}`,
     });
   }
 );
